c1afa60576b24b2a47d38f345db09188a97fb341
samkaplan5571 <49130995+samkaplan5571@users.noreply.github.com>
2019-07-20 14:40:51 -0400

    Add files via upload
    Project materials included

---
+++ CreateTable.sql
@@ -0,0 +1,19 @@
+DROP TABLE restaurant;
+
+
+Create Table restaurant (
+
+restaurant VARCHAR(255),
+cuisine VARCHAR(255),
+price VARCHAR(255),
+food_rating INT ,
+dining_style VARCHAR(255),
+dress_code VARCHAR(255),
+grade VARCHAR,
+zipcode VARCHAR(255),
+criticalviolationcount INT,
+notcriticalviolationcount INT 
+);
+
+
+select * from restaurant
